2a9974d815746a48633e0efe3fb1e28c6085f80b
roy2pushkar <roy2000pushkar.com>
2022-07-01 11:45:05 +0530

    Day1

---
+++ Day1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int start = 0;
+        int end = n-1;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            else if (k>arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+  // } Driver Code Ends
